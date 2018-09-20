6ae7538faccda6b790328546edffe181c89d262b
wan03 <38962452+wan03@users.noreply.github.com>
2018-09-20 17:06:59 -0400

    Cleaned up js code and try to locate the bug with no success

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,30 +28,10 @@ var answers = [
   "Hockey"
 ]
 
-
-
-
-
-
-
-
-// Do the logic for the time remaining
-function interlude () {
-
-if (smallTime == 0){
-  stopSmallRun();
-  time = 20;
-  smallTime= 6;
-  // $(".result").html($result);
-  $(".fact").addClass("hidden")
-  $(".question" + currentQuestion).removeClass("hidden");
-  run();
-  // $("#win").html("<p> You got " + correct + " correct questions </p>" + "<p> You got " + incorrect + " incorrect questions</p>" + "You ran out of time in " + outTime + " questions")
- };
-};
+// Reset Function
 
 function resetFunction (){
-   
+  
   if (reset == 10) {
     $(".question9").addClass("hidden");
     $("#win").html("<p> You got " + correct + " correct questions </p>" + "<p> You got " + incorrect + " incorrect questions</p>" + "You ran out of time in " + outTime + " questions")
@@ -60,19 +40,42 @@ function resetFunction (){
     incorrect = 0;
     outTime = 0;
     currentQuestion = 1;
+    reset = 16;
+    time = 20;
+    smalltime = 6;
     $("#win").addClass("hidden");
-   stopReset();
-   game();
- };
-
+    stopReset();
+    stopSmallRun();
+    stopTimeRemaining(); 
+    console.log("reset has fired");   
+    game();
+  };
+  
 };
+
+
+// Functions that set the intervals. One for the quesion time, one for the interlude between questions and one for the final countdown to reset.
+
+  function interlude () {
+  
+  if (smallTime == 0){
+    stopSmallRun();
+    time = 20;
+    smallTime= 6;
+    $(".fact").addClass("hidden")
+    $(".question" + currentQuestion).removeClass("hidden");
+    console.log("small has fired");
+    run();  
+   };
+  };
 
 function noTime () {
   if (time == 0) {
   currentQuestion++
   outTime++
   $result = "You ran out of time! The correct answer is: "  + answers[currentQuestion];
-    moveQuestion();
+  console.log("no time has fired");
+  moveQuestion();
   };
 };
 
@@ -94,7 +97,6 @@ intervalId = setInterval(timeRemaining, 1000);
 
 function timeBetween () {
   smallTime--
-  // $("#time2").text("Time Remaining: " + smallTime + " seconds");
   interlude();
 };
 
@@ -123,220 +125,102 @@ function stopReset () {
 };
 
 
-// Dinamically insert questions? Maybe make it so the order is randomized every time. Decided not to go with this and just have the questions and asnwers in the HTML. Here I'm going to create the logic that will handle showing and hiding eac question.
+// Function to handle showing and hiding each question.
 
 function moveQuestion () {
-
+console.log('first question ? ' + currentQuestion);console.log("move has fired");
   if (currentQuestion == 1) {
     time = 20;
-    $(".question1").removeClass("hidden");
-    // console.log(currentQuestion + "1")
-    // currentQuestion++      
+    $(".question1").removeClass("hidden");     
   } if (currentQuestion == 2){  
-    // time = 20;
     stopTimeRemaining();  
-    // console.log("Attempted to move")
-    // console.log(currentQuestion + "2")
-    $(".question1").addClass("hidden");
-    // var temp = $('<img src="assets/images/toystory.jpg" alt="Toy Story">');   
-    // $(".fact").append(temp);
+    $(".question1").addClass("hidden"); 
     $(".fact").removeClass("hidden")
-    // console.log($result);
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/toystory.jpg" alt="Toy Story">')
-
-    // $(".result").html($result);
-    smallRun();
-    // interlude();
-    // $("#time2").text("Time Remaining: " + smallTime + " seconds");
-    // if (smallTime == 0){
-    //   stopSmallRun();
-    //   time = 10;
-    //   $("#fact").addClass("hidden")
-    //   $(".question2").removeClass("hidden");
-    //  };
-    // currentQuestion++
+    smallRun();  
   } if (currentQuestion == 3){
-    // console.log(currentQuestion + "3")
     $(".question2").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/walle.jpg" alt="Wall-E">');
     $(".result").html($result);
-    smallRun();
-    // interlude();   
-    //  if (smallTime == 0){
-    //   $("#fact").addClass("hidden")
-    //   $(".question3").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
+    smallRun();  
   } if (currentQuestion == 4){
     $(".question3").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/mrincredible.jpeg" alt="Mr. Incredible">');
-    smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question4").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
+    smallRun();   
   } if (currentQuestion == 5){
     $(".question4").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/radiatorsprings.jpeg" alt="Radiator Springs">');
     smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question5").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
   } if (currentQuestion == 6){
     $(".question5").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/paradisefalls.jpg" alt="Paradise Falls">');
     smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question6").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
   } if (currentQuestion == 7){
     $(".question6").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/ratatouille.jpg" alt="Paris">');
     smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question7").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
   } if (currentQuestion == 8){
     $(".question7").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/mater.jpg" alt="Spy">');
     smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question8").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
   } if (currentQuestion == 9){
     $(".question8").addClass("hidden");
     time = 20;
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/marlin.png" alt="Marlin">');
     smallRun();
-    //  if (smallTime == 0){
-    //   $(".fact").addClass("hidden")
-    //   $(".question9").removeClass("hidden");
-    //   stopSmallRun();
-    //  };
-    // currentQuestion++
   } if (currentQuestion == 10){
+    $(".question9").addClass("hidden");
     $(".fact").removeClass("hidden")
     $(".fact").html('<p>'+ $result + '</p>' + '<img src="assets/images/riley.jpg" alt="Hockey">');
     smallRun();
-    //  if (smallTime == 0){
-    //    $("#win").html("<p> You got " + correct + " correct questions </p>" + "<p> You got " + incorrect + " incorrect questions</p>" + "You ran out of time in " + outTime + " questions")
-      //  stopSmallRun();
-       timeReset();
-      //  if (reset == 0) {
-      //    currentQuestion = 1;
-      //   stopReset();
-      //   moveQuestion();
-      //  };
-    //  };
-    
+    timeReset();    
   };
-
 };
 
-
-
-
-
-
-
-
-
-
-// Detect when right/wrong answer is and add to var also detect if the player has run out of time.
+// Detect when right/wrong answer is and display that in the DOM
 
 function checkAnswer () {
 $(".answer").on("click", function () {
-  // $resultQuestion =  $(".question" + currentQuestion).children(".correct").text();
-  // console.log($resultQuestion);  
   if ($(this).hasClass("correct")) {
+    console.log("check+ has fired");
     correct++
     currentQuestion++
     $result = "You are correct!"
     moveQuestion();
-    // console.log(correct)
-    
-
-
   } else {
+    console.log("check - has fired");
     incorrect++
     currentQuestion++
     console.log(currentQuestion);
     $result = "You are incorrect! The correct answer is: " + answers[currentQuestion];
     moveQuestion();
-    // console.log(incorrect)
   };
 });
 };
 
-
-
-
-
-//Once all questions have been answered or time has elapsed remove everything from the page and show the stats of how the player did. 
-
 // Function to run the game
 
 function game () {
+  console.log("game has fired");
   run();
   moveQuestion();
-  checkAnswer();
- 
+  checkAnswer(); 
 };
 
+// Click event listener to start the game when Start button is pressed.
 
 $("#start").on("click", game);
-
-
-
-
-//Make it so that questions appear one at a time.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
